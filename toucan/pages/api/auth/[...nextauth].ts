a0833ebd2a3935c4bf196729f520448699658858
iamjianyoudai <daijianyou@gmail.com>
2024-11-21 15:00:15 +0100

    update auth file

--- toucan/pages/api/auth/[...nextauth].ts
+++ toucan/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,19 @@
-import AzureADProvider from 'next-auth/providers/azure-ad';
 import NextAuth from 'next-auth';
+import AzureADProvider from 'next-auth/providers/azure-ad';
 
 export default NextAuth({
   providers: [
     AzureADProvider({
-      // TODO: Handle missing variables by properly throwing error
       clientId: process.env.AZURE_AD_CLIENT_ID || '',
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
       tenantId: process.env.AZURE_AD_TENANT_ID,
+      authorization: {
+        params: {
+          scopes: process.env.AZURE_AD_SCOPES?.split(',') || ['openid', 'profile', 'email'],
+        },
+      },
     }),
   ],
+
+  debug: true,
 });
